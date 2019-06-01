46ae8ef1b217a5cf5af341e159f4bcf29365d228
dntjr8096 <lkjs8269@naver.com>
2019-06-02 00:21:40 +0900

    Add Draw Function

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
 let painting = false;
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "rgb(0,0,0)";
+ctx.lineWidth = 2.5;
 
 function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-  painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
